94ab2b80c4a0e811178831b71214889269e5362b
raflisalamudin <ahmadraflisalamudin1928@gmail.com>
2023-11-21 22:44:03 +0700

    new caitlinhalderman

---
+++ caitlinhalderman/main.js
@@ -0,0 +1,12 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   new TypeIt(".type", {
+//     strings: ["Gamer"],
+//     speed: 250,
+//     loop: true,
+//   })
+//     .pause(1000)
+//     .delete(5)
+//     .type("Youtuber")
+//     .pause(1000)
+//     .go();
+// });
